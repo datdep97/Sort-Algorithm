1e69d30b21195faa7df3a658929feb248f08d663
phichidung <dungpc@sphinx-software.com>
2019-03-26 10:58:15 +0700

    Chidung

--- Sort-Algorithm.php
+++ Sort-Algorithm.php
@@ -9,15 +9,25 @@
         public function sorting($arr);
     }
 
-
-    //Kim viet anh dang làm ve thuat toan sap xep chon.
-    class Slectionsort implements Sort_Algorithm{
+    // Chi Dung work to Insertion Sort
+    class InsertionSort implements Sort_Algorithm{
         public function sorting($arr){
-            
+            $total = count($arr);
+            for ($i = 0; $i < $total; $i++)
+            {
+                $loop = $i;
+                $current = $arr[$i];
+                while($loop > 0 && ($arr[$loop - 1] > $current))
+                {
+                    $arr[$loop] = $arr[$loop - 1];
+                    $loop -= 1;
+                }
+                $arr[$loop] = $current;
+            }
+            return print_r($arr);        
         }
     }
-
+ 
     $sortingSolution = new Sorting_Solution;
-    echo $sortingSolution->sort(new Slectionsort(), [1,2,3,4,5]);
-
+    echo $sortingSolution->sort(new InsertionSort(), [99,54,17,200,3]);
 ?>
