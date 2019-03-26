cefbd8a07144e1c7ebc149979d551b8ef1a6a551
huong9x <huongnv@sphinx-software.com>
2019-03-26 10:17:19 +0700

    Bubble Sort

--- Sort-Algorithm.php
+++ Sort-Algorithm.php
@@ -1,5 +1,28 @@
-<?php
+,<?php
 
-    echo "hello homie !";
+class Sorting_Solution {
+    public function sort (Sort_Algorithm $sort_algorith, $arr) {
+        return $sort_algorith->sorting($arr);
+    }
+}
+interface Sort_Algorithm {
+    public function sorting($arr);
+}
+class Bubble implements Sort_Algorithm {
+    public function sorting ($arr) {
+        $size = count($arr)-1;
+        for ($i=0; $i<$size; $i++) {
+            for ($j=0; $j<$size-$i; $j++) {
+                $k = $j+1;
+                if ($arr[$k] < $arr[$j]) {
+                    list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
+                }
+            }
+        }
+        return $arr;
+    }
+}
+$s = new Sorting_Solution();
+print_r($s->sort(new Bubble(), [7,9,2,8,6,4,2]));
 
 ?>
