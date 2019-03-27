59221a9121ea233f362fef1261dc17a2fc54d607
hailong1606 <hailong160697@gmail.com>
2019-03-27 09:55:02 +0700

    hailong

--- Sort-Algorithm.php
+++ Sort-Algorithm.php
@@ -10,14 +10,54 @@
     }
 
 
-    //Kim viet anh dang làm ve thuat toan sap xep chon.
-    class Slectionsort implements Sort_Algorithm{
-        public function sorting($arr){
-            
+    //Hai Long dang làm ve thuat toan sap xep
+    class Insertionsort implements Sort_Algorithm
+    {
+        public function sorting($arr)
+        {
+            for($i=1;$i<count($arr);$i++)
+            {
+            $j = $i-1;
+            $t = $arr[$i];
+            while ($j>=0&&($t < $arr[$j])) {
+                $arr[$j + 1] = $arr[$j];
+                $j--;
+            }
+            $arr[$j + 1] = $t;
+            }
+            return $arr;
         }
     }
+    class Quicksort implements Sort_Algorithm
+    {
+        function sorting($arr)
+        {
+            if(count($arr)<2)
+            {
+                return $arr;
+            }
+            else
+            {
 
-    $sortingSolution = new Sorting_Solution;
-    echo $sortingSolution->sort(new Slectionsort(), [1,2,3,4,5]);
+                $le = $ri = array();
+                $pivol = array_shift($arr)
+                foreach($arr as $val)
+                {
+                    if($val > $pivol)
+                    {
+                        $ri[] = $val;
+                    }
+                    else
+                    {
+                        $le[] = $val;
+                    }
+                }
+                return array_merge($this->sorting($le),array($pivol),$this->sorting($ri));
+                
+            }
+        }
+    }
+    $s = new Sorting_Solution;
+    print_r($s->sort(new Quicksort(), [10,-30,84,28,50,11,1]));
 
 ?>
