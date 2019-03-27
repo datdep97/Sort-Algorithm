ed87e9ab6b26081a87bd8aab34947426555dd010
datdep97 <datlt@sphinx-software.com>
2019-03-27 10:40:21 +0700

    ladat commit this file

--- Sort-Algorithm.php
+++ Sort-Algorithm.php
@@ -1,23 +1,89 @@
 <?php
-    class Sorting_Solution{
-        public function sort(Sort_Algorithm $sort_algorithm, $arr){
-            return $sort_algorithm->sorting($arr);
+class Sorting_Solution {
+    public function sort (Sort_Algorithm $sort_algorith, $arr) {
+        return $sort_algorith->sorting($arr);
+    }
+}
+interface Sort_Algorithm {
+    public function sorting($arr);
+}
+class BubbleSort implements Sort_Algorithm {
+    public function sorting ($arr) {
+        $size = count($arr)-1;
+        for ($i=0; $i<$size; $i++) {
+            for ($j=0; $j<$size-$i; $j++) {
+                $k = $j+1;
+                if ($arr[$k] < $arr[$j]) {
+                    list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
+                }
+            }
+        }
+        return $arr;
+    }
+}
+class SelectionSort implements Sort_Algorithm{
+    public function sorting($arr){
+        $total = count($arr);
+        for($i = 0; $i < $total; $i++){
+            $min = $i;
+            for ($j = $i + 1; $j < $total; $j++){
+                if ($arr[$j] < $arr[$min]){
+                    $min = $j;
+                }
+            }
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$min];
+            $arr[$min] = $temp;
+        }
+        return $arr;
+    }
+}
+class InsertionSort implements Sort_Algorithm{
+    public function sorting($arr){
+        $total = count($arr);
+        for ($i = 0; $i < $total; $i++)
+        {
+            $loop = $i;
+            $current = $arr[$i];
+            while($loop > 0 && ($arr[$loop - 1] > $current))
+            {
+                $arr[$loop] = $arr[$loop - 1];
+                $loop -= 1;
+            }
+            $arr[$loop] = $current;
+        }
+        return print_r($arr);        
+    }
+}
+class QuickSort implements Sort_Algorithm{
+    public function sorting($arr){
+        $length = count($arr);
+
+if($length <= 1){
+    return $arr;
+}
+else{
+
+    $pivot = $arr[0];
+    
+    $left = $right = array();
+    
+    for($i = 1; $i < count($arr); $i++)
+    {
+        if($arr[$i] < $pivot){
+            $left[] = $arr[$i];
+        }
+        else{
+            $right[] = $arr[$i];
         }
     }
-
-    interface Sort_Algorithm{
-        public function sorting($arr);
+        return array_merge($this->sorting($left), array($pivot), $this->sorting($right));
     }
-
-
-    //Kim viet anh dang làm ve thuat toan sap xep chon.
-    class Slectionsort implements Sort_Algorithm{
-        public function sorting($arr){
-            
-        }
-    }
-
-    $sortingSolution = new Sorting_Solution;
-    echo $sortingSolution->sort(new Slectionsort(), [1,2,3,4,5]);
-
+}
+}
+$abc = new Sorting_Solution();
+print_r($abc->sort(new BubbleSort(), [2,1,3,8,6,9,4]));
+print_r($abc->sort(new SelectionSort(), [2,1,3,8,6,9,4]));
+print_r($abc->sort(new QuickSort(), [2,1,3,8,6,9,4]));
+print_r($abc->sort(new InsertionSort(), [2,1,3,8,6,9,4]));
 ?>
