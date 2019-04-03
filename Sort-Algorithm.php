6eccfad6b8cfb10162a17f4d4e841aea8137be34
Huong Nguyen <huongnguye2@my.smccd.edu>
2019-04-03 10:16:46 +0700

    Update Sort-Algorithm.php

--- Sort-Algorithm.php
+++ Sort-Algorithm.php
@@ -1,50 +1,47 @@
 <?php
-   class Sorting_Solution {
-     public function register(Sort_Algorithm $sort_algorithm) {
-       $this->sort_algorithm = $sort_algorithm;
-     }
-     public function sort($arr) {
-       $size = count($arr);
-       if($this->sort_algorithm->canHandle($size)){
-         print_r($this->sort_algorithm->sorting($arr));
-       } else
-       throw new Exception('Exceeding the permitted limits!');
-     }
-   }
-
-   interface Sort_Algorithm {
-       public function canHandle($limit);
-       public function sorting($arr);
-   }
-
-   class BubbleSort implements Sort_Algorithm {
-       public function canHandle($limit) {
-           if ($limit > 0 && $limit  < 10) {
-               return true;
-           } else
-               return false;
-       }
-       public function sorting($arr) {
-           $size = count($arr) - 1;
-           for ($i = 0;$i < $size;$i++) {
-               for ($j = 0;$j < $size - $i;$j++) {
-                   $k = $j + 1;
-                   if ($arr[$k] < $arr[$j]) {
-                       list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
-                   }
-               }
-           }
-           return $arr;
-       }
-   }
-       
-   class SelectionSort implements Sort_Algorithm {
-       public function canHandle($limit) {
-           if ($limit > 10 && $limit  < 15) {
-               return true;
-           } else
-               return false;
-       }
+class Sorting_Solution {
+    public $newsort = array();
+    public function register(Sort_Algorithm $sort_algorithm) {
+        array_push($this->newsort, $sort_algorithm);
+    }
+    public function sort($arr) {
+        $size = count($arr);
+        for ($i = 0;$i < count($this->newsort);$i++) {
+            if ($this->newsort[$i]->canHandle($size)) {
+                print_r($this->newsort[$i]->sorting($arr));
+            }
+        }
+    }
+}
+interface Sort_Algorithm {
+    public function canHandle($limit);
+    public function sorting($arr);
+}
+class BubbleSort implements Sort_Algorithm {
+    public function canHandle($limit) {
+        if ($limit > 0 && $limit < 10) {
+            return true;
+        } else return false;
+    }
+    public function sorting($arr) {
+        $size = count($arr) - 1;
+        for ($i = 0;$i < $size;$i++) {
+            for ($j = 0;$j < $size - $i;$j++) {
+                $k = $j + 1;
+                if ($arr[$k] < $arr[$j]) {
+                    list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
+                }
+            }
+        }
+        return $arr;
+    }
+}
+class SelectionSort implements Sort_Algorithm {
+    public function canHandle($limit) {
+        if ($limit > 10 && $limit < 15) {
+            return true;
+        } else return false;
+    }
     public function sorting($arr) {
         $total = count($arr);
         for ($i = 0;$i < $total;$i++) {
@@ -61,14 +58,12 @@
         return $arr;
     }
 }
-
 class InsertionSort implements Sort_Algorithm {
-       public function canHandle($limit) {
-           if ($limit > 15 && $limit  < 20) {
-               return true;
-           } else
-               return false;
-       }
+    public function canHandle($limit) {
+        if ($limit > 15 && $limit < 20) {
+            return true;
+        } else return false;
+    }
     public function sorting($arr) {
         $total = count($arr);
         for ($i = 0;$i < $total;$i++) {
@@ -83,15 +78,11 @@ class InsertionSort implements Sort_Algorithm {
         return $arr;
     }
 }
-
 $ss = new Sorting_Solution();
 $ss->register(new BubbleSort);
 $ss->register(new SelectionSort);
 $ss->register(new InsertionSort);
-try {
-    $ss->sort([2, 1, 3, 8, 6, 9, 41, 3, 8, 6, 9, 41, 3, 8, 6, 9, 41, 3, 8, 6, 9, 4]);
-} catch(Exception $e) {
-  echo 'Warning: ' .$e->getMessage();
-}
-
+$ss->sort([1, 5, 4, 8, 6, 9]);
+$ss->sort([1, 5, 4, 8, 6, 9, 1, 5, 4, 8, 6, 9, 1, 5, 4, 8, 6, 9, 1, 5, 4, 8, 6, 9, 1, 5, 4, 8, 6, 9]);
+$ss->sort([1, 5, 4, 8, 6, 9, 1, 5, 4, 8, 6, 9, 1, 5, 4, 8, 6, 9]);
 ?>
